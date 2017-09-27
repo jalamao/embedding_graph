c2a7d5eacb23061cb034ea5ea6ce09e52338f1e8
Blaz Skrlj <skrljblaz@gmail.com>
2017-09-27 14:58:21 +0200

    Working base pipe

--- algorithms/minitest.py
+++ algorithms/minitest.py
@@ -1,15 +1,16 @@
 ## this is a basic test of hinmine capabilities
 
 from cf_netSDM.library_cli import *
+import numpy as np
 
 ## load test gml and read it further on..
 heuristic = "rf"
-cycle = {'node_list': [u'movie', u'person'], 'edge_list': [u'features', u'acts_in']}
+cycle = {'node_list': [u'movie', u'person'], 'edge_list': [u'directed_by', u'directed']}
 example_net = load_gml("../data/imdb_gml.gml","_")
 decomposed = hinmine_decompose(example_net,heuristic,cycle=cycle)
 propositionalized = hinmine_propositionalize(decomposed)
 
-print(propositionalized['train_features']['data'])
-print(propositionalized['train_features']['target'])
-
+data = propositionalized['train_features']
+print(len(data['target_names']))
+print(np.count_nonzero(data['target']))
 ## minitests
