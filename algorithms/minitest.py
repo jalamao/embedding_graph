316729f16fb850ae9dea1e14cd0467e5f668c486
Blaz Skrlj <skrljblaz@gmail.com>
2017-09-27 14:32:45 +0200

    Added basic exmaple

--- algorithms/minitest.py
+++ algorithms/minitest.py
@@ -3,10 +3,13 @@
 from cf_netSDM.library_cli import *
 
 ## load test gml and read it further on..
-heuristic = "delta"
-cycle = {'node_list': [u'movie', u'person', u'movie'], 'edge_list': [u'features', u'acts_in']}
+heuristic = "rf"
+cycle = {'node_list': [u'movie', u'person'], 'edge_list': [u'features', u'acts_in']}
 example_net = load_gml("../data/imdb_gml.gml","_")
 decomposed = hinmine_decompose(example_net,heuristic,cycle=cycle)
 propositionalized = hinmine_propositionalize(decomposed)
-print(propositionalized)
 
+print(propositionalized['train_features']['data'])
+print(propositionalized['train_features']['target'])
+
+## minitests
