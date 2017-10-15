b13fbe6a91faf43da0b36e7a4d600c8007e5779e
SkBlaz <skrljblaz@gmail.com>
2017-10-15 21:40:34 +0200

    Added parallel pagerank

--- algorithms/libHIN/embeddings.py
+++ algorithms/libHIN/embeddings.py
@@ -3,8 +3,16 @@ from dataStructures import HeterogeneousInformationNetwork
 from core import stochastic_normalization, page_rank
 import numpy as np
 
-def hinmine_embedding(hin,use_decomposition=True):
+def pr_kernel(index_row):
+    pr = page_rank(graph, [index_row], try_shrink=True)
+    norm = np.linalg.norm(pr, 2)
+    if norm > 0:
+        pr = pr / np.linalg.norm(pr, 2)
+        return (index_row,pr)
+                
+def hinmine_embedding(hin,use_decomposition=True, parallel=4):
 
+    global graph
     # embed the input network to a term matrix    
     assert isinstance(hin, HeterogeneousInformationNetwork)
 
@@ -24,15 +32,25 @@ def hinmine_embedding(hin,use_decomposition=True):
             
         graph = stochastic_normalization(converted)
 
+    ## initialize
     vectors = np.zeros((n, n))
 
-    ## to se da paralelno!
-    for index in range(n):
-        pr = page_rank(graph, [index], try_shrink=True)
-        norm = np.linalg.norm(pr, 2)
-        if norm > 0:
-            pr = pr / np.linalg.norm(pr, 2)
-            vectors[index, :] = pr
+    if parallel > 0:
+        import multiprocessing as mp
+        p = mp.Pool(processes=parallel)
+        results = p.map(pr_kernel,range(n))
+        for pr_vector in results:
+            vectors[pr_vector[0],:] = pr_vector[1]
+    
+    else:
+        ## to se da paralelno!
+        for index in range(n):
+            pr = page_rank(graph, [index], try_shrink=True)
+            norm = np.linalg.norm(pr, 2)
+            if norm > 0:
+                pr = pr / np.linalg.norm(pr, 2)
+                vectors[index, :] = pr
+            
     train_features = {
         'data': vectors[hin.train_indices, :],
         'target': hin.label_matrix[hin.train_indices, :],
