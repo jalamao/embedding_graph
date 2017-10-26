afe88b37568bef7921e7bfc89341f8a2090d6a40
Blaz Skrlj <skrljblaz@gmail.com>
2017-10-26 14:49:42 +0200

    removed sparse matrices in the last part

--- algorithms/libHIN/embeddings.py
+++ algorithms/libHIN/embeddings.py
@@ -55,7 +55,7 @@ def hinmine_embedding(hin,use_decomposition=True, parallel=True,return_type="raw
 
     ## initialize
 
-    if n > 5000:
+    if n > 50000:
         vectors = sp.csr_matrix((n, n))
     else:
         vectors = np.zeros((n, n))
@@ -73,8 +73,8 @@ def hinmine_embedding(hin,use_decomposition=True, parallel=True,return_type="raw
         for enx, pr_vector in enumerate(results):
             if pr_vector != None:
                 vectors[pr_vector[0],:] = pr_vector[1]
-            if enx % 1000 == 0:
-                vectors = vectors.tocsr()
+            #if enx % 1000 == 0:
+            #    vectors = vectors.tocsr()
     else:
         if verbose:
             emit_state("Non-Parallel embedding in progress..")
